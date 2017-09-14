37841b1a9ace16afaf28a2893e745c0b69d0c43f
yakato <frey.kasia@gmail.com>
2017-09-14 17:22:35 +0200

    Styles for tiles

--- src/components/App/Board/index.js
+++ src/components/App/Board/index.js
@@ -8,17 +8,17 @@ class Board extends Component {
     return (
       <div className={styles.boardWrapper}>
         <div className={styles.board}>
-          <Tile />
-          <Tile />
-          <Tile />
-          <Tile />
-          <Tile />
-          <Tile />
-          <Tile />
-          <Tile />
-          <Tile />
-          <Tile />
-          <Tile />
+          <Tile value="2"/>
+          <Tile value="4"/>
+          <Tile value="8"/>
+          <Tile value="16"/>
+          <Tile value="32"/>
+          <Tile value="64"/>
+          <Tile value="128"/>
+          <Tile value="256"/>
+          <Tile value="512"/>
+          <Tile value="1024"/>
+          <Tile value="2048"/>
           <Tile />
           <Tile />
           <Tile />
--- src/components/App/Tile/index.js
+++ src/components/App/Tile/index.js
@@ -3,9 +3,16 @@ import React, { Component } from 'react'
 import styles from './styles.scss'
 
 class Tile extends Component {
+
+  generateClass (value) {
+    return value ? `tile${value}` : ''
+  }
+
   render() {
+    const tileNumber = this.generateClass(this.props.value)
     return (
-      <div className={styles.tile}>
+      <div className={styles.tile + ' ' + styles[tileNumber]}>
+        {this.props.value}
       </div>
     )
   }
