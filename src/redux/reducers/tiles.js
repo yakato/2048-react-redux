2434ce9b4bba0766623a64e79d512349ddf0a174
yakato <frey.kasia@gmail.com>
2017-09-19 17:20:20 +0200

    Start with move logic in reducers

--- src/redux/reducers/tiles.js
+++ src/redux/reducers/tiles.js
@@ -6,9 +6,25 @@ import {
 
 const initialState = {
   tilesById: {},
+  tilesByColumn: {
+    1: [],
+    2: [],
+    3: [],
+    4: [],
+  },
+  tilesByRow: {
+    1: [],
+    2: [],
+    3: [],
+    4: [],
+  },
+  tilesByPosition: {},
   emptyCells: [],
   numberOfTiles: 0,
 }
+
+const getTileById = (state, tileId) => (state.tilesById[tileId])
+const generatePositionKey = (x,y) => (`${x}::${y}`)
 
 export default (state = initialState, action) => {
   switch (action.type) {
@@ -24,27 +40,76 @@ export default (state = initialState, action) => {
 
     case CREATE_TILE:
       const randomEmptyCell = state.emptyCells[Math.floor(Math.random()*state.emptyCells.length)]
-      const newEmptyCellsArray = state.emptyCells
+      const newEmptyCellsArray = [ ...state.emptyCells ]
       newEmptyCellsArray.splice(newEmptyCellsArray.indexOf(randomEmptyCell),1)
       const id = state.numberOfTiles + 1
       const newTile = {
         x: randomEmptyCell.x,
         y: randomEmptyCell.y,
-        value: '2'
+        value: '2',
+        id: id,
       }
+      const position = generatePositionKey(newTile.x, newTile.y)
       return {
         ...state,
         emptyCells: newEmptyCellsArray,
-        tilesById : { ...state.tilesById, [id]: newTile },
-        numberOfTiles: id
+        tilesById : {
+          ...state.tilesById,
+          [id]: newTile
+        },
+        numberOfTiles: id,
+        tilesByRow: {
+          ...state.tilesByRow,
+          [newTile.x]: [...state.tilesByRow[newTile.x], id]
+        },
+        tilesByColumn: {
+          ...state.tilesByColumn,
+          [newTile.y]: [...state.tilesByColumn[newTile.y], id]
+        },
+        tilesByPosition: {
+          ...state.tilesByPosition,
+          [position]: id
+        }
       }
 
     case MOVE_TILES:
-      const updatedTiles = state.tilesById
-      Object.keys(updatedTiles).map((key, index) =>  {
-        return console.log(updatedTiles[key].x, updatedTiles[key].y)
-      })
-      return state
+      const direction = action.payload.direction
+      const newState = { ...state }
+      if(direction === 'right') {
+        //y 321
+        for (let y = 3; y > 0; y--) {
+          newState.tilesByColumn[y].forEach((tileId) => {
+            let currentTile = getTileById(newState, tileId)
+            if(currentTile) {
+              while(currentTile.y + 1 <= 4 && !newState.tilesByPosition[generatePositionKey(currentTile.x, currentTile.y+1)]) {
+                const oldPosition = generatePositionKey(currentTile.x, currentTile.y)
+                newState.emptyCells = [ ...newState.emptyCells, { x: currentTile.x, y: currentTile.y } ]
+                console.log(currentTile.y)
+                currentTile.y = currentTile.y + 1
+                console.log(currentTile.y)
+
+                newState.emptyCells = newState.emptyCells.filter(
+                  (obj) => {
+                    const a = { x: currentTile.x, y: currentTile.y }
+                    console.log(obj, a)
+                    return obj !== { x: currentTile.x, y: currentTile.y }})
+                const newPosition = generatePositionKey(currentTile.x, currentTile.y)
+                //remove unused positions
+
+                newState.tilesByPosition[oldPosition] = undefined
+                //remove currentTile from previous position
+                newState.tilesByColumn[y] = newState.tilesByColumn[y].filter((id) => id !== currentTile.id)
+                //add to current columns
+                newState.tilesByColumn[currentTile.y] = [...newState.tilesByColumn[currentTile.y], currentTile.id]
+                //update positions
+                newState.tilesByPosition[newPosition] = currentTile.id
+
+              }
+            }
+          })
+        }
+      }
+      return newState
     default:
       return state
   }
