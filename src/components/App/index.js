1c4b1166e073e7c3e35bc4234cefdede8fe65d82
yakato <frey.kasia@gmail.com>
2017-09-21 13:22:30 +0200

    Listen keydown

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -7,8 +7,23 @@ import styles from './styles.scss'
 
 class App extends Component {
 
-  handleTest(e, direction) {
-    this.props.moveTiles(direction)
+  handleKeyDown(event) {
+    event.preventDefault()
+    switch(true) {
+      case event.keyCode === 37: return this.props.moveTiles('left')
+      case event.keyCode === 38: return this.props.moveTiles('up')
+      case event.keyCode === 39: return this.props.moveTiles('right')
+      case event.keyCode === 40: return this.props.moveTiles('down')
+      default: break
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown.bind(this))
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    window.removeEventListener('keydown', this.handleKeyDown.bind(this))
   }
 
   render() {
@@ -16,7 +31,6 @@ class App extends Component {
     return (
       <article className={styles.app}>
         <header className={styles.header}>2048 in React</header>
-        <button onClick={(e) => this.handleTest(e, direction)}>Test</button>
         <Board />
       </article>
     )
